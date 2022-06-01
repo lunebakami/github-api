b9f5b578f08aedda504945026fa98dd15ccc60da
Luis Santos <luis.santos.taiba@gmail.com>
2022-06-01 12:19:25 -0300

    feat: get all users endpoin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const api = require('./services/api');
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get('/api/', (req, res) => {
   return res.send('Hello World');
+});
+
+app.get('/api/users', async (req, res) => {
+  try {
+    const { since = 0 } = req.query;
+
+    const { data } = await api.get(`/users?since=${since}`);
+
+    res.json({
+      nextPage: `/api/users?since=${Number(since) + 30}`,
+      data,
+    });
+  } catch (error) {
+    res.json({ data: error });
+  }
 });
 
 app.listen(3000, () => {
